9e68f3b54f2f1d40c2d7bde5d6927a9d3ba1367a
Thanawat Potidet <thanawatpotidech@gmail.com>
2025-03-17 18:19:53 +0700

    modify lsp

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -175,7 +175,16 @@ return {
           capabilities = capabilities,
         },
         jdtls = {},
-        -- rust_analyzer = {},
+        svelte = {},
+        rust_analyzer = {
+          settings = {
+            ['rust-analyzer'] = {
+              cargo = { allFeatures = true },
+              checkOnSave = { command = 'clippy' },
+            },
+          },
+        },
+        csharp_ls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
