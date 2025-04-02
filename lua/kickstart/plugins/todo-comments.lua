f91694378a094ce8217e78345b347f83a1d6ecb7
Thanawat Potidet <thanawatpotidech@gmail.com>
2025-04-03 03:45:55 +0700

    update: todo-comments

--- lua/kickstart/plugins/todo-comments.lua
+++ lua/kickstart/plugins/todo-comments.lua
@@ -1,5 +1,24 @@
 -- Highlight todo, notes, etc in comments
 return {
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  {
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = {
+      signs = true,
+      keywords = {
+        FIX = {
+          icon = '? ', -- icon used for the sign, and in search results
+          color = 'error', -- can be a hex color, or a named color (see below)
+          alt = { 'FIXME', 'BUG', 'FIXIT', 'ISSUE' }, -- removed SAFETY from here
+        },
+        SAFETY = {
+          icon = '? ',
+          color = 'error',
+        },
+      },
+    },
+  },
 }
+
 -- vim: ts=2 sts=2 sw=2 et
