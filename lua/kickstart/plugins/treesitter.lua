bebb7331a5daa2a41b5d57c24ac8cb627fd918c6
Thanawat Potidet <thanawatpotidech@gmail.com>
2026-05-09 14:43:25 +0700

    update tree sitter

--- lua/kickstart/plugins/treesitter.lua
+++ lua/kickstart/plugins/treesitter.lua
@@ -7,12 +7,31 @@ return {
     config = function()
       -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
       -- Install parsers on startup
-      require('nvim-treesitter').install({ 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'rust' })
+      require('nvim-treesitter').install {
+        'bash',
+        'c',
+        'diff',
+        'html',
+        'lua',
+        'luadoc',
+        'markdown',
+        'markdown_inline',
+        'query',
+        'vim',
+        'vimdoc',
+        'rust',
+        'typescript',
+        'javascript',
+        'go',
+        'java',
+        'c_sharp',
+      }
 
       -- Highlighting and indent are enabled by default via Neovim's built-in treesitter.
       -- Enable treesitter-based folding (optional)
       vim.o.foldmethod = 'expr'
       vim.o.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+      vim.o.foldlevel = 99
     end,
   },
 }
